f54458ee7366d10688407b03b7d1d83df6542933
blackops97 <blackops97@users.noreply.github.com>
2016-09-17 05:06:12 +0000

    Update ar_version.lua

--- plugins/ar_version.lua
+++ plugins/ar_version.lua
@@ -1,9 +1,9 @@
 --[[ 
 ▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
 ▀▄ ▄▀                                      ▀▄ ▄▀ 
-▀▄ ▄▀    BY SAJJAD HUSSIEN                 ▀▄ ▄▀ 
-▀▄ ▄▀   BY SAJJADHUSSIEN   (@TH3_Evil)     ▀▄ ▄▀    
-▀▄ ▄  JUST WRITED BY SAJJAD HUSSIEN        ▀▄ ▄▀ 
+▀▄ ▄▀    BY @illOlli                 ▀▄ ▄▀ 
+▀▄ ▄▀   BY @illOlli              ▀▄ ▄▀    
+▀▄ ▄  JUST WRITED BY  @illOlli       ▀▄ ▄▀ 
 ▀▄ ▄▀      VIRSON      :  الاصدار           ▀▄ ▄▀  
 ▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
 --]]
@@ -12,7 +12,7 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-  local S = '🌐 #السورس TH3_Evil\n🔰 الاصدار v5.0 ☑️\n🌐 الموقع\n \nhttps://github.com/blackops97/TH3_Evil\n🔧 لمطور @TH3_Evil \n 🔧📱 قناة البوت @TH3Evil_iq'
+  local S = 'ߌР#السورس KING TELE\nߔ࠘癄اصدار v6.0 ☑️\nߌРالموقع\n \nhttps://github.com/blackops97/KING.TELE\nߔ砙ęŘ癈رين @illOlli \n @Sadikal_knani10 \n ߔ簟ӱ قناة البوت @KINGTELE1'
 reply_msg(reply_id, S, ok_cb, false)
 end
 
