986d49df3ed08cdca28f9b617d83e7adb1de9eaa
blackops97 <blackops97@users.noreply.github.com>
2016-09-17 16:39:57 +0000

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -120,6 +120,18 @@ else
     echo "Run $0 install"
     exit 1
   fi
+  fi
+  
+  chmod 777 TH3_Evil.lua
+  
+  #Adding some color. By @MehdiHS
+   echo -e "\033[38;5;208m"
+   echo -e "     > DEV : SAJJAD HUSSIEN                                          "
+   echo -e "     > DEV : @TH3_Evil                                           "
+   echo -e "     > Bot ID : DEV : @sajjad_aliraqi_bot                       "
+   echo -e "     > Github : https://github.com/blackops97/TH3_Evil    "
+   echo -e "          BY sajjad hussien                                   \033[0;00m"
+   echo -e "\e[36m"
   rm -r ../.telegram-cli/state #Prevent tg from crash
-  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/TH3_EVil.lua -l 1 -E $@
+  ./tg/bin/telegram-cli -k ./tg/tg-server.pub -s ./bot/TH3_Evil.lua -l 1 -E $@
 fi
